2cdd13a403dc5efaff45bc37fa1880b3437ddeb2
Vilius Paskonis <vilius.paskonis@gmail.com>
2025-03-21 01:13:00 +0200

    Remove ApplocationRole from IdentityCore middleware. Implement AuthService

--- WorthBoards.Business/Services/AuthService.cs
+++ WorthBoards.Business/Services/AuthService.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using WorthBoards.Business.Dtos.Identity;
 using WorthBoards.Business.Services.Interfaces;
+using WorthBoards.Business.Utils.Interfaces;
 using WorthBoards.Data.Identity;
 
 namespace WorthBoards.Business.Services
@@ -8,17 +12,48 @@ namespace WorthBoards.Business.Services
     public class AuthService(
         UserManager<ApplicationUser> userManager,
         SignInManager<ApplicationUser> signInManager,
-        RoleManager<ApplicationRole> roleManager
+        IMapper mapper,
+        ITokenGenerator tokenGenerator
     ) : IAuthService
     {
-        public Task<UserLoginResponse> LoginUserAsync(UserLoginRequest credentials)
+        public async Task<UserLoginResponse> LoginUserAsync(UserLoginRequest credentials)
         {
-            throw new NotImplementedException();
+            var user = await userManager.FindByNameAsync(credentials.UserName);
+
+            // TODO: Add custom exception handle
+            ArgumentNullException.ThrowIfNull(user);
+
+            var result = await signInManager.CheckPasswordSignInAsync(user, credentials.Password, false);
+
+            // TODO: Add custom exception handle
+            if (!result.Succeeded)
+                throw new UnauthorizedAccessException("Invalid Login credentials.");
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName ?? string.Empty),
+                new Claim(ClaimTypes.Email, user.Email ?? string.Empty)
+            };
+
+            var jwtToken = tokenGenerator.GenerateJwtToken(claims);
+            
+            return new UserLoginResponse(user.Id, credentials.UserName, jwtToken);
         }
 
-        public Task<UserResponse> RegisterUserAsync(UserRegisterRequest registerUser)
+        public async Task<UserResponse> RegisterUserAsync(UserRegisterRequest registerUser)
         {
-            throw new NotImplementedException();
+            var user = mapper.Map<ApplicationUser>(registerUser);
+
+            user.CreationDate = DateTime.UtcNow;
+
+            var response = await userManager.CreateAsync(user, registerUser.Password);
+
+            // TODO: Add custom exception handle
+            if (!response.Succeeded)
+                throw new DbUpdateException("Failed to register user");
+
+            return mapper.Map<UserResponse>(user);
         }
     }
 }
--- WorthBoards.Data/DependencyInjection.cs
+++ WorthBoards.Data/DependencyInjection.cs
@@ -10,14 +10,12 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //Register stuff for data layer here
-
             var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? configuration.GetConnectionString("WorthBoardsConnection");
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(connectionString, npgsqlOptions => npgsqlOptions.MigrationsAssembly("WorthBoards.Data")));
 
-            var builder = services.AddIdentityCore<ApplicationUser>(options =>
+            services.AddIdentityCore<ApplicationUser>(options =>
             {
                 // Password settings.
                 options.Password.RequireDigit = true;
@@ -35,12 +33,8 @@ namespace Microsoft.Extensions.DependencyInjection
                 // User settings.
                 options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
                 options.User.RequireUniqueEmail = true;
-            });
-
-            builder = new IdentityBuilder(builder.UserType, typeof(ApplicationRole), builder.Services);
-            builder
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddRoleManager<RoleManager<ApplicationRole>>()
                 .AddSignInManager<SignInManager<ApplicationUser>>()
                 .AddDefaultTokenProviders();
 

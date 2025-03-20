a04b5e11946901321108c8d5f1c062d6995d8d58
Vilius Paskonis <vilius.paskonis@gmail.com>
2025-03-20 22:27:14 +0200

    Add AutoMapper to Business layer. Map User

---
+++ WorthBoards.Business/AutoMapper/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WorthBoards.Business.Dtos.Identity;
+using WorthBoards.Data.Identity;
+
+namespace WorthBoards.Business.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // User
+            CreateMap<ApplicationUser, UserResponse>();
+            CreateMap<UserRequest, ApplicationUser>();
+        }
+    }
+}

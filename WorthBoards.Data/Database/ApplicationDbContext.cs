92ca017b466905e99142b38bfddfc479b71b4066
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2025-03-20 16:51:52 +0200

    DB Setup

--- WorthBoards.Data/Database/ApplicationDbContext.cs
+++ WorthBoards.Data/Database/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
-﻿
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using POS_System.Data.Database;
 using WorthBoards.Data.Identity;
+using WorthBoards.Domain.Entities;
 
 namespace WorthBoards.Data.Database
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : IdentityDbContext<ApplicationUser, ApplicationRole, int>(options)
     {
+        //DB Sets
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<BoardOnUser> BoardOnUsers { get; set; }
+        public DbSet<BoardTask> BoardTasks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<InvitationData> InvatationData { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<NotificationOnUser> NotificationsOnUsers { get; set; }
+        public DbSet<TaskOnUser> TasksOnUsers { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Linker.LinkAll(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

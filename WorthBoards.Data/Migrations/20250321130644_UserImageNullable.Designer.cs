72ecdb03d2af9b482e6e8b58bc7b952260b66c27
Vilius Paskonis <vilius.paskonis@gmail.com>
2025-03-21 15:10:39 +0200

    Change User ImageURL to nullable

--- WorthBoards.Data/Migrations/20250321130644_UserImageNullable.Designer.cs
+++ WorthBoards.Data/Migrations/20250321130644_UserImageNullable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WorthBoards.Data.Database;
@@ -11,9 +12,11 @@ using WorthBoards.Data.Database;
 namespace WorthBoards.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250321130644_UserImageNullable")]
+    partial class UserImageNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

5c64e378c6c3f74e740148b9a18e1b9c01c5a02d
MatasKvn <matas.kvainauskas@protonmail.com>
2025-04-01 22:55:47 +0300

    Create NotificationController & Get endpoint

---
+++ WorthBoards.Api/Controllers/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WorthBoards.Business.Dtos.Identity;
+using WorthBoards.Business.Dtos.Responses;
+using WorthBoards.Business.Services.Interfaces;
+
+namespace WorthBoards.Api.Controllers;
+
+[Route("api/notifications")]
+[ApiController]
+public class NotificationController(INotificationService _notificationService, IMapper _mapper) : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpGet("/{userId:int}")]
+    public async Task<List<NotificationResponse>> GetNotificationsByUserId(int userId)
+    {
+        var notifications = await _notificationService.GetNotificationsByUserId(userId);
+
+        return notifications;
+    }
+}

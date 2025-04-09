564437e62800d3259a60db9eea32ea3b81f64f87
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2025-04-09 19:51:29 +0300

    Board api updates. Delete, Put, Get one.

--- client/src/api/board.api.ts
+++ client/src/api/board.api.ts
@@ -1,17 +1,22 @@
 import {Board} from '@/types/types'
-import TaskApi from '@/api/task.api'
 import { FetchResponse, HTTPMethod, PagedResponse } from '../types/fetch'
 import { apiBaseUrl } from '../constants/api'
 import { fetch, getAuthorizedHeaders } from '../utils/fetch'
 
-export type CreateBoardDto = Omit<Board, 'id' | 'creationDate'> & {
-    imageFile?: File
-}
+export type CreateBoardDto = Omit<Board, 'id' | 'creationDate'>
 
 export default class BoardApi {
     static async getBoards(pageNumber: number): Promise<FetchResponse<PagedResponse<Board>>> {
         return await fetch({
             url: `${apiBaseUrl}/boards?pageNum=${pageNumber}`,
+            method: HTTPMethod.GET,
+            headers: getAuthorizedHeaders()
+        })
+    }
+
+    static async getBoardById(id: number): Promise<FetchResponse<Board>> {
+        return await fetch({
+            url: `${apiBaseUrl}/boards/${id}`,
             method: HTTPMethod.GET,
             headers: getAuthorizedHeaders()
         })
@@ -26,14 +31,20 @@ export default class BoardApi {
         })
     }
 
-    static async getBoardById(id: number): Promise<Board> {
-        return {
-            title: 'test',
-            id,
-            description: 'test description',
-            imageURL: 'https://preview.colorkit.co/color/ff0000.png?static=true',
-            creationDate: new Date(Date.now()),
-            tasks: (await TaskApi.getTasks(id)).items
-        }
+    static async updateBoard(id: number, board: CreateBoardDto): Promise<FetchResponse<Board>> {
+        return await fetch({
+            url: `${apiBaseUrl}/boards/${id}`,
+            method: HTTPMethod.PUT,
+            headers: getAuthorizedHeaders(),
+            body: JSON.stringify(board)
+        })
+    }
+
+    static async deleteBoard(id: number): Promise<FetchResponse<any>> {
+        return await fetch({
+            url: `${apiBaseUrl}/boards/${id}`,
+            method: HTTPMethod.DELETE,
+            headers: getAuthorizedHeaders()
+        })
     }
 }

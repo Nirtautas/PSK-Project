d0cd99ab96e44e70bc779ba3cd32f166389f2ad3
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2025-04-16 21:05:36 +0300

    Added pagination and method declarations to interface.

--- WorthBoards.Data/Repositories/Interfaces/ICommentRepository.cs
+++ WorthBoards.Data/Repositories/Interfaces/ICommentRepository.cs
@@ -1,9 +1,17 @@
-﻿using WorthBoards.Data.Repositories.Base;
+﻿using WorthBoards.Data.Identity;
+using WorthBoards.Data.Repositories.Base;
 using WorthBoards.Domain.Entities;
 
 namespace WorthBoards.Data.Repositories.Interfaces
 {
     public interface ICommentRepository : IRepository<Comment>
     {
+        Task<(IReadOnlyList<Tuple<Comment, ApplicationUser>> Results, int TotalCount)> GetTaskCommentsWithUsernameAndPicture(
+            int taskId,
+            int pageSize,
+            int pageNumber,
+            CancellationToken cancellationToken = default);
+
+        Task<Tuple<Comment, ApplicationUser>?> GetCommentWithUsernameAndPicture(int commentId, CancellationToken cancellationToken);
     }
 }

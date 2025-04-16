b592071c0a1c5057903142aea7ea3e1774c68815
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2025-04-16 20:52:50 +0300

    Comment with user data.

--- WorthBoards.Data/Repositories/CommentRepository.cs
+++ WorthBoards.Data/Repositories/CommentRepository.cs
@@ -2,10 +2,41 @@
 using WorthBoards.Domain.Entities;
 using WorthBoards.Data.Repositories.Base;
 using WorthBoards.Data.Database;
+using Microsoft.EntityFrameworkCore;
+using WorthBoards.Data.Identity;
 
 namespace WorthBoards.Data.Repositories
 {
     public class CommentRepository(ApplicationDbContext dbContext) : Repository<Comment>(dbContext), ICommentRepository
     {
+        public async Task<IEnumerable<Tuple<Comment, ApplicationUser>>> GetTaskCommentsWithUsernameAndPicture(int taskId, CancellationToken cancellationToken)
+        {
+            var commentsLinkedToTaskQuery =
+                _dbContext.Comments
+                .Where(comment => comment.BoardTaskId == taskId)
+                .Join(
+                    _dbContext.Users,
+                    comment => comment.UserId,
+                    user => user.Id,
+                    (comment, user) => Tuple.Create(comment, user)
+                );
+
+            return await commentsLinkedToTaskQuery.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Tuple<Comment, ApplicationUser>?> GetCommentWithUsernameAndPicture(int commentId, CancellationToken cancellationToken)
+        {
+            var commentLinkedToUserQuery =
+                _dbContext.Comments
+                .Where(comment => comment.Id == commentId)
+                .Join(
+                    _dbContext.Users,
+                    comment => comment.UserId,
+                    user => user.Id,
+                    (comment, user) => Tuple.Create(comment, user)
+                );
+
+            return await commentLinkedToUserQuery.FirstOrDefaultAsync(cancellationToken);
+        }
     }
 }

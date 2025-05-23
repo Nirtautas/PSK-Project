aa7a2ea5e677f9eebf661702f8776b262b1ca5aa
MatasKvn <matas.kvainauskas@protonmail.com>
2025-05-23 21:10:14 +0300

    Create backend image upload validation

--- WorthBoards.Business/Services/FileService.cs
+++ WorthBoards.Business/Services/FileService.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using WorthBoards.Business.Services.Interfaces;
+using WorthBoards.Business.Validators;
 using WorthBoards.Common.Constrants;
 
 namespace WorthBoards.Business.Services;
@@ -8,6 +9,8 @@ public class FileService : IFileService
 {
     public async Task<string> UploadImage(IFormFile image)
     {
+        FileUploadValidator.ValidateImage(image);
+
         var fileExtension = Path.GetExtension(image.FileName);
         var filename = Guid.NewGuid().ToString() + fileExtension;
         var imagePath = Path.Combine(ImageFiles.STATIC_IMAGE_DIR, filename);
